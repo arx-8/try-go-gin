d76a949b55f4d67381e304226f08f2ca994b62f0
arx-8 <swans.randomness@gmail.com>
2021-10-23 23:20:36 +0900

    with middleware

--- example.go
+++ example.go
@@ -8,9 +8,18 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	ua := ""
+
+	r.Use(func(c *gin.Context) {
+		ua = c.GetHeader("user-agent")
+		c.Next()
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message":    "pong",
+			"user-agent": ua,
 		})
 	})
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
