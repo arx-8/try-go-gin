1484403faeba48c2962010cc50ccc969513dc5b1
arx-8 <swans.randomness@gmail.com>
2021-10-23 23:13:14 +0900

    Specify port

--- example.go
+++ example.go
@@ -9,7 +9,7 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 	// You can access `http://localhost:8080/ping`
 }
